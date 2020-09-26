bfbb7810d55ed5d7b24d69d9e874fe28e9cfe998
Raif Serkan Albayrak <serkan.albayrak@ieu.edu.tr>
2020-09-26 12:11:14 +0300

    mvc'ye dönüştürmenin adımlarını attık. Ama henüz routing yapmadık.

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller //Controller'ı sonradan ekliyoruz. Bu şekilde controller oluyor.
+    {
+        public string Index()
+        {
+            return "index sayfası";
+        }
+    }
+}
